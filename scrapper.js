8b87f8764447b45dbf575f09b4b5fa2bf4c68bc3
Jose Ruas <joseluis.ruas@gmail.com>
2019-06-05 22:40:11 -0300

    Temporary solution

--- scrapper.js
+++ scrapper.js
@@ -5,7 +5,7 @@ const puppeteer = require("puppeteer");
   const page = await browser.newPage();
   const baseURL = 'https://n.rivals.com/prospect_rankings/rivals150/';
   const baseYear = 2003;
-  const maxYear = 2019;
+  const maxYear = 2021;
 
   let actualYear = baseYear;
   const allPlayers = [];
@@ -14,7 +14,8 @@ const puppeteer = require("puppeteer");
   let actualPlayersInformations;
   
   
-  while (baseYear < maxYear+1) {
+  while (actualYear < maxYear+1) {
+    await page.goto(actualURL);
     await page.goto(actualURL);
     await page.goto(actualURL);
     await page.goto(actualURL);
@@ -32,7 +33,8 @@ const puppeteer = require("puppeteer");
           commitCollege: tr.querySelector("td.school-name-prospects").innerText.trim(),
         }))
     );
-    console.log(actualPlayersInformations.length);
+
+    console.log(actualPlayersInformations.length, actualYear);
 
     for (const actualPlayer of actualPlayersInformations) {
       actualPlayer.year = actualYear;
@@ -42,41 +44,10 @@ const puppeteer = require("puppeteer");
     actualYear += 1;
     actualURL = baseURL + actualYear;
   }
-  
-
-  
-
-
-
   // format text
   // name \n => espaço
   // location \n => espaço
   // commitCollege pegar até a primeira \
-
-  
-  
- 
-  await page.goto(actualURL);
-
-  actualPlayersInformations = await page.evaluate(() =>
-    Array.from(document.querySelectorAll("table.sortable-table.rankings.list.gradient-long tbody tr"))
-      .map(tr => ({
-        name: tr.querySelector("td.player-name-prospects").innerText.trim(),
-        rank: tr.querySelector("td.rank").innerText.trim(),
-        position: tr.querySelector("td.position").innerText.trim(),
-        location: tr.querySelector("td.location-prospects").innerText.trim(),
-        commitCollege: tr.querySelector("td.school-name-prospects").innerText.trim(),
-      }))
-  );
-
-  console.log(actualPlayersInformations.length);
-
-  for (const actualPlayer of actualPlayersInformations) {
-    actualPlayer.year = actualYear;
-    allPlayers.push(actualPlayer);
-  }
-
-  //console.log(allPlayers);
 
   await browser.close();
 })();
